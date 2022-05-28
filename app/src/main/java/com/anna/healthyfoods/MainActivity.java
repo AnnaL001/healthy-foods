7e016e07a13880fb49e80a85565591357fce2c0b
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-28 07:02:12 +0300

    Add initial project structure

---
+++ app/src/main/java/com/anna/healthyfoods/MainActivity.java
@@ -0,0 +1,18 @@
+package com.anna.healthyfoods;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.anna.healthyfoods.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+  private ActivityMainBinding binding;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    binding = ActivityMainBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
+  }
+}
