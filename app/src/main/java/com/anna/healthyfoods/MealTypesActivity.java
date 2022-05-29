8a7f932b05f8c34dcfecda82734dc533fde7b814
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-29 19:25:05 +0300

    Add logic for displaying user's name

--- app/src/main/java/com/anna/healthyfoods/MealTypesActivity.java
+++ app/src/main/java/com/anna/healthyfoods/MealTypesActivity.java
@@ -3,12 +3,20 @@ package com.anna.healthyfoods;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.anna.healthyfoods.databinding.ActivityMealTypesBinding;
 
 public class MealTypesActivity extends AppCompatActivity {
+  public static final String TAG = MealTypesActivity.class.getSimpleName();
+  private ActivityMealTypesBinding binding;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_meal_types);
+    binding = ActivityMealTypesBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
+    Log.i(TAG, "MealTypesActivity opened");
+    binding.welcomeText.setText(getString(R.string.welcome, getIntent().getStringExtra("name")));
   }
 }
