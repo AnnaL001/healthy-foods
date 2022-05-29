8745b8c49067566865697789932f694b6bc9394f
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-29 22:20:12 +0300

    Add logic to display recipe list

--- app/src/main/java/com/anna/healthyfoods/RecipeListActivity.java
+++ app/src/main/java/com/anna/healthyfoods/RecipeListActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.anna.healthyfoods.adapter.RecipeListAdapter;
+import com.anna.healthyfoods.databinding.ActivityRecipeListBinding;
+
 public class RecipeListActivity extends AppCompatActivity {
+  private ActivityRecipeListBinding binding;
+  private String[] recipeTitles = {"Chicken Vesuvio", "Green Bean Casserole Pie Recipe", "Baked Sweet Potatoes and Pecans", "Vegetable Lasagna", "Fresh tuna tortillas"};
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_recipe_list);
+    binding = ActivityRecipeListBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
+
+    initializeAdapter();
+  }
+
+  private void initializeAdapter(){
+    RecipeListAdapter adapter = new RecipeListAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, recipeTitles);
+    binding.recipeListView.setAdapter(adapter);
   }
 }
