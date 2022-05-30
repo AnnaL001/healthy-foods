3f062ffa5d1c3cf5aad9c265494211fcefb4bd8d
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-30 19:21:53 +0300

    Update User Interface logic

--- app/src/main/java/com/anna/healthyfoods/UserDetailsActivity.java
+++ app/src/main/java/com/anna/healthyfoods/UserDetailsActivity.java
@@ -39,20 +39,37 @@ public class UserDetailsActivity extends AppCompatActivity {
       List<String> diets = UserInterfaceHelpers.getSelectedChips(binding.dietChipGroup);
       List<String> allergies = UserInterfaceHelpers.getSelectedChips(binding.allergyChipGroup);
 
-      // Validate user input before populating intent extras
-      if(Validator.validateRequiredUserDetailsFormInput(name, diets, allergies)){
-        if(Validator.validateCorrectNameInput(name)){
-          intent.putExtra("name", name);
-          startActivity(intent);
-          Log.i(TAG, "Navigating to MealTypesActivity...");
-        } else {
-          Toast.makeText(getApplicationContext(), getString(R.string.toast_correct_name_input), Toast.LENGTH_SHORT).show();
-          Log.e(TAG, "Error: Name contains less than four characters");
-        }
-      } else {
-        Toast.makeText(getApplicationContext(), getString(R.string.toast_required_input), Toast.LENGTH_SHORT).show();
-        Log.e(TAG, "Error: All input values are required");
-      }
+      // Validate user input
+      validateFormInputs(intent, name, diets, allergies);
     });
   }
+
+  // Validate user input before populating intent extras
+  private void validateFormInputs(Intent intent, String name, List<String> diets, List<String> allergies){
+    if(Validator.validateRequiredUserDetailsFormInput(name, diets, allergies)){
+      if(Validator.validateCorrectNameInput(name)){
+        intent.putExtra("name", name);
+
+        // Clear form after capturing data
+        clearFormInputs();
+
+        startActivity(intent);
+
+        Log.i(TAG, "Navigating to MealTypesActivity...");
+      } else {
+        Toast.makeText(getApplicationContext(), getString(R.string.toast_correct_name_input), Toast.LENGTH_SHORT).show();
+        Log.e(TAG, "Error: Name contains less than four characters");
+      }
+    } else {
+      Toast.makeText(getApplicationContext(), getString(R.string.toast_required_input), Toast.LENGTH_SHORT).show();
+      Log.e(TAG, "Error: All input values are required");
+    }
+  }
+
+  private void clearFormInputs(){
+    UserInterfaceHelpers.clearFormInput(binding.nameTextInputLayout);
+    UserInterfaceHelpers.clearFormInput(binding.dietChipGroup);
+    UserInterfaceHelpers.clearFormInput(binding.allergyChipGroup);
+  }
+
 }
