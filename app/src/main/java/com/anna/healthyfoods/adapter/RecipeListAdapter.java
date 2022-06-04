761aa35ea0e13e1c132b89c12474c67dfa21303f
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-06-04 08:42:50 +0300

    Remove unused code

--- app/src/main/java/com/anna/healthyfoods/adapter/RecipeListAdapter.java
+++ app/src/main/java/com/anna/healthyfoods/adapter/RecipeListAdapter.java
@@ -8,26 +8,6 @@ import androidx.annotation.Nullable;
 
 import java.util.Locale;
 
-public class RecipeListAdapter extends ArrayAdapter {
-  private Context context;
-  private String[] recipeTitles;
-  private int no = 1;
+public class RecipeListAdapter {
 
-  public RecipeListAdapter(@NonNull Context context, int resource, String[] recipeTitles) {
-    super(context, resource);
-    this.context = context;
-    this.recipeTitles = recipeTitles;
-  }
-
-  @Override
-  public int getCount() {
-    return recipeTitles.length;
-  }
-
-  @Nullable
-  @Override
-  public Object getItem(int position) {
-    String recipeTitle = recipeTitles[position];
-    return String.format(Locale.ENGLISH,"%d. %s", no++, recipeTitle);
-  }
 }
