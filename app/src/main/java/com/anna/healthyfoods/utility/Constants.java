1bf6fa4197590e24366b7973e9bb45443ca990ef
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-06-05 12:43:12 +0300

    Add API constants

---
+++ app/src/main/java/com/anna/healthyfoods/utility/Constants.java
@@ -0,0 +1,11 @@
+package com.anna.healthyfoods.utility;
+
+import com.anna.healthyfoods.BuildConfig;
+
+public class Constants {
+  public static final String EDAMAM_API_ID = BuildConfig.EDAMAM_API_ID;
+  public static final String EDAMAM_API_KEY = BuildConfig.EDAMAM_API_KEY;
+  public static final String EDAMAM_API_BASE_URL = "https://api.edamam.com/api/recipes/";
+  public static final String API_ID_QUERY_PARAMETER = "app_id";
+  public static final String MEAL_TYPE_QUERY_PARAMETER = "mealType";
+}
