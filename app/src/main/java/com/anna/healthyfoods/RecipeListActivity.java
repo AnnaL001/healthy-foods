d2f9a45760681bb21de062c9e55f8bff50667902
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-06-06 01:04:35 +0300

    Add recyclerview with linear layout to display recipes

--- app/src/main/java/com/anna/healthyfoods/RecipeListActivity.java
+++ app/src/main/java/com/anna/healthyfoods/RecipeListActivity.java
@@ -1,44 +1,108 @@
 package com.anna.healthyfoods;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.View;
 
 import com.anna.healthyfoods.adapter.RecipeListAdapter;
+import com.anna.healthyfoods.client.EdamamClient;
 import com.anna.healthyfoods.databinding.ActivityRecipeListBinding;
+import com.anna.healthyfoods.interfaces.EdamamApi;
+import com.anna.healthyfoods.models.RecipeSearchResponse;
+import com.anna.healthyfoods.models.Settings;
+import com.anna.healthyfoods.utility.Constants;
+
+import org.parceler.Parcels;
 
 import java.util.Objects;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RecipeListActivity extends AppCompatActivity {
   public static final String TAG = RecipeListActivity.class.getSimpleName();
   private ActivityRecipeListBinding binding;
-  private final String[] recipeTitles = {"Chicken Vesuvio", "Green Bean Casserole Pie Recipe", "Baked Sweet Potatoes and Pecans", "Vegetable Lasagna", "Fresh tuna tortillas"};
+  private RecipeListAdapter adapter;
 
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     binding = ActivityRecipeListBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
 
-    String title = getString(getIntent().getIntExtra("meal_type", R.string.breakfast));
-    Log.d("RecipeListActivity", String.format("Displaying %s recipes", title));
+    String mealType = getIntent().getStringExtra("meal_type");
+    Settings userSettings = Parcels.unwrap(getIntent().getParcelableExtra("userSettings"));
 
-    // Set Action Bar title to meal type passed via intent
-    Objects.requireNonNull(this.getSupportActionBar()).setTitle(title);
+    // Set action bar title to passed meal type
+    Objects.requireNonNull(this.getSupportActionBar()).setTitle(mealType);
 
-    initializeAdapter();
-    initializeOnClickListener();
+    // Create arrays for diet and health preferences for recipes
+    String[] diets = new String[userSettings.getDiets().size()];
+    String[] preferences = new String[userSettings.getPreferences().size()];
+
+    EdamamApi client = EdamamClient.getClient();
+    Call<RecipeSearchResponse> call = client.getRecipesByMealType("public", "breakfast", Constants.EDAMAM_API_ID, Constants.EDAMAM_API_KEY, mealType, userSettings.getDiets().toArray(diets), userSettings.getPreferences().toArray(preferences));
+
+    loadRecipes(call);
   }
 
-  private void initializeAdapter(){
-    // Add code to initialize adapter
+  private void loadRecipes(Call<RecipeSearchResponse> call){
+    call.enqueue(new Callback<RecipeSearchResponse>() {
+      @Override
+      public void onResponse(@NonNull Call<RecipeSearchResponse> call, @NonNull Response<RecipeSearchResponse> response) {
+        hideProgressBar();
+
+        if(response.isSuccessful()){
+          assert response.body() != null;
+          adapter = new RecipeListAdapter(RecipeListActivity.this, response.body().getHits());
+          binding.recipeList.setLayoutManager(new LinearLayoutManager(RecipeListActivity.this));
+          binding.recipeList.setAdapter(adapter);
+
+          if(adapter.getItemCount() > 0){
+            showRecipes();
+          } else {
+            showNoRecipesFound();
+          }
+        } else {
+          showUnsuccessfulFeedback();
+        }
+      }
+
+      @Override
+      public void onFailure(@NonNull Call<RecipeSearchResponse> call, @NonNull Throwable t) {
+        hideProgressBar();
+        showFailureFeedback();
+        Log.e(TAG, "Error: ", t);
+      }
+    });
   }
 
-  // Initialize listener for clicks on list items
-  private void initializeOnClickListener(){
-    // Add code for click listener
+  private void hideProgressBar(){
+    binding.progressBar.setVisibility(View.GONE);
+  }
+
+  private void showRecipes(){
+    binding.recipeList.setVisibility(View.VISIBLE);
+  }
+
+  private void showUnsuccessfulFeedback(){
+    binding.errorText.setText(getString(R.string.unsuccessful_feedback));
+    binding.errorText.setVisibility(View.VISIBLE);
+  }
+
+  private void showFailureFeedback(){
+    binding.errorText.setText(getString(R.string.failure_feedback));
+    binding.errorText.setVisibility(View.VISIBLE);
+  }
+
+  private void showNoRecipesFound(){
+    binding.errorText.setText(getString(R.string.no_recipes_found));
+    binding.errorText.setVisibility(View.VISIBLE);
   }
 }
