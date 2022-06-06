0279c6e5719a6c6fd90452d69f499c7779b2eb30
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-06-06 17:05:46 +0300

    Add swiping of views with tab layout

--- app/src/main/java/com/anna/healthyfoods/HomeActivity.java
+++ app/src/main/java/com/anna/healthyfoods/HomeActivity.java
@@ -9,9 +9,13 @@ import com.anna.healthyfoods.adapter.HomeViewPagerAdapter;
 import com.anna.healthyfoods.databinding.ActivityHomeBinding;
 import com.anna.healthyfoods.models.Settings;
 import com.anna.healthyfoods.ui.MealTypeFragment;
+import com.anna.healthyfoods.ui.SearchFragment;
+import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
 import org.parceler.Parcels;
+
+import java.util.Objects;
 
 public class HomeActivity extends AppCompatActivity {
   private ActivityHomeBinding binding;
@@ -27,6 +31,24 @@ public class HomeActivity extends AppCompatActivity {
     userSettings = Parcels.unwrap(getIntent().getParcelableExtra("userSettings"));
     viewPagerAdapter = new HomeViewPagerAdapter(this, userSettings);
     binding.viewPager.setAdapter(viewPagerAdapter);
+
+    initializeTabLayout();
+  }
+
+  private void initializeTabLayout(){
+    new TabLayoutMediator(binding.tabLayout, binding.viewPager, (tab, position) -> {
+      switch (position){
+        case 0:
+          tab.setText(getString(R.string.meal_types));
+          break;
+        case 1:
+          tab.setText(getString(R.string.search));
+          break;
+        case 2:
+          tab.setText(getString(R.string.starred));
+          break;
+      }
+    }).attach();
   }
 
 }
