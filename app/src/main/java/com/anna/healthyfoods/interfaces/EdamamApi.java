1b489791e68414dc01d897a6a34bfed38065805c
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-06-07 07:56:30 +0300

    Add functionality to fetch recipes by id

--- app/src/main/java/com/anna/healthyfoods/interfaces/EdamamApi.java
+++ app/src/main/java/com/anna/healthyfoods/interfaces/EdamamApi.java
@@ -1,11 +1,13 @@
 package com.anna.healthyfoods.interfaces;
 
 import com.anna.healthyfoods.models.Hit;
+import com.anna.healthyfoods.models.Recipe;
 import com.anna.healthyfoods.models.RecipeSearchResponse;
 import com.anna.healthyfoods.utility.Constants;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface EdamamApi {
@@ -27,5 +29,13 @@ public interface EdamamApi {
           @Query(Constants.API_KEY_QUERY_PARAMETER) String apikey,
           @Query(Constants.DIET_LABEL_QUERY_PARAMETER) String[] diets,
           @Query(Constants.HEALTH_LABEL_QUERY_PARAMETER) String[] health
+  );
+
+  @GET("v2/{id}")
+  Call<Hit> getRecipeById(
+          @Path("id") String recipeId,
+          @Query(Constants.SEARCH_TYPE_QUERY_PARAMETER) String recipeSearch,
+          @Query(Constants.APP_ID_QUERY_PARAMETER) String appId,
+          @Query(Constants.API_KEY_QUERY_PARAMETER) String apiKey
   );
 }
