c0607863741af718b3e75cb6ae0c6a149bffbf79
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-06-11 22:53:29 +0300

    Add additional form validation

--- app/src/main/java/com/anna/healthyfoods/utility/Validator.java
+++ app/src/main/java/com/anna/healthyfoods/utility/Validator.java
@@ -1,6 +1,13 @@
 package com.anna.healthyfoods.utility;
 
+import android.content.Context;
+import android.util.Patterns;
+
+import com.anna.healthyfoods.R;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Validator {
   // Returns false if form input values are empty and name is less than four characters
@@ -9,7 +16,53 @@ public class Validator {
   }
 
   // Returns false if name is less than four characters
-  public static boolean validateCorrectNameInput(String name){
+  public static boolean validateNameInput(String name){
     return !(name.length() < 4 || name.matches(".*\\d.*"));
+  }
+
+  public static boolean validateEmailInput(String email) {
+    return email != null && Patterns.EMAIL_ADDRESS.matcher(email).matches();
+  }
+
+  public static Map<String, Object> validatePasswordInput(Context context, String password) {
+    Map<String, Object> result = new HashMap<>();
+    result.put("valid", false);
+
+    if(!(password.length() < 8)){
+      result.put("message", context.getString(R.string.required_password_length_error));
+    } else if (!(hasDigits(password))){
+      result.put("message", context.getString(R.string.required_password_with_digit_error));
+    } else if (!(hasUppercase(password))){
+      result.put("message", context.getString(R.string.required_password_with_uppercase_error));
+    } else if (!(hasLowercase(password))){
+      result.put("message", context.getString(R.string.required_password_with_lowercase_error));
+    } else if(!(hasSymbols(password))){
+      result.put("message", context.getString(R.string.required_password_with_special_char_error));
+    } else {
+      result.put("valid", true);
+    }
+
+    return result;
+  }
+
+  public static boolean validateConfirmPasswordInput(String password, String confirmPassword) {
+    return confirmPassword.equals(password);
+  }
+
+
+  private static boolean hasDigits(String text) {
+    return text.matches(".*\\d.*");
+  }
+
+  private static boolean hasUppercase(String text) {
+    return text.matches("[A-Z]");
+  }
+
+  private static boolean hasLowercase(String text) {
+    return text.matches("[a-z]");
+  }
+
+  private static boolean hasSymbols(String text) {
+    return text.matches("[!@#$%^&*()-_+=?><.,;:\"'`~]");
   }
 }
