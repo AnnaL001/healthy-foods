31b30f3d101f5eb526d26a700674020278bbb808
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-06-13 01:10:53 +0300

    Add firebase ui dependency for FirebaseRecyclerAdapter

---
+++ app/src/main/java/com/anna/healthyfoods/ui/SavedRecipesFragment.java
@@ -0,0 +1,104 @@
+package com.anna.healthyfoods.ui;
+
+import static com.anna.healthyfoods.utility.UserInterfaceHelpers.hideProgressBar;
+import static com.anna.healthyfoods.utility.UserInterfaceHelpers.showNoContentFound;
+import static com.anna.healthyfoods.utility.UserInterfaceHelpers.showRecipes;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.anna.healthyfoods.R;
+import com.anna.healthyfoods.databinding.FragmentSavedRecipesBinding;
+import com.anna.healthyfoods.databinding.ItemRecipeListBinding;
+import com.anna.healthyfoods.models.Recipe;
+import com.anna.healthyfoods.utility.Constants;
+import com.anna.healthyfoods.viewholder.FirebaseRecipeViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
+
+public class SavedRecipesFragment extends Fragment {
+  private FragmentSavedRecipesBinding binding;
+  private FirebaseRecyclerAdapter<Recipe, FirebaseRecipeViewHolder> firebaseAdapter;
+  private DatabaseReference recipeReference;
+
+  public SavedRecipesFragment() {
+  }
+
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    binding = FragmentSavedRecipesBinding.inflate(inflater, container, false);
+    return binding.getRoot();
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+
+    String userId = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+    recipeReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RECIPE_LOCATION).child(userId);
+
+    //For now, until database connection is added
+    setUpFirebaseAdapter();
+    hideProgressBar(binding.progressBar);
+    showRecipes(binding.starredRecipeList);
+  }
+
+  private void setUpFirebaseAdapter(){
+    // Set up FirebaseAdapter
+    FirebaseRecyclerOptions<Recipe> options =
+            new FirebaseRecyclerOptions.Builder<Recipe>()
+                    .setQuery(recipeReference, Recipe.class)
+                    .build();
+
+    firebaseAdapter = new FirebaseRecyclerAdapter<Recipe, FirebaseRecipeViewHolder>(options) {
+
+      @NonNull
+      @Override
+      public FirebaseRecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new FirebaseRecipeViewHolder(ItemRecipeListBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+      }
+
+      @Override
+      protected void onBindViewHolder(@NonNull FirebaseRecipeViewHolder holder, int position, @NonNull Recipe recipe) {
+        holder.bindRecipe(recipe);
+      }
+    };
+
+    binding.starredRecipeList.setLayoutManager(new LinearLayoutManager(getContext()));
+    if (firebaseAdapter.getItemCount() < 1) {
+      showNoContentFound(binding.errorText, requireContext(), getString(R.string.no_saved_recipes));
+    }
+    binding.starredRecipeList.setAdapter(firebaseAdapter);
+  }
+
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+    binding = null;
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+    firebaseAdapter.startListening();
+  }
+
+  @Override
+  public void onStop() {
+    firebaseAdapter.stopListening();
+    super.onStop();
+  }
+}
---
+++ app/src/main/java/com/anna/healthyfoods/viewholder/FirebaseRecipeViewHolder.java
@@ -0,0 +1,64 @@
+package com.anna.healthyfoods.viewholder;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.anna.healthyfoods.R;
+import com.anna.healthyfoods.RecipeDetailsActivity;
+import com.anna.healthyfoods.databinding.ItemRecipeListBinding;
+import com.anna.healthyfoods.interfaces.ItemOnClickListener;
+import com.anna.healthyfoods.models.Recipe;
+import com.anna.healthyfoods.utility.Constants;
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
+
+public class FirebaseRecipeViewHolder extends RecyclerView.ViewHolder implements ItemOnClickListener {
+  public static final String TAG = FirebaseRecipeViewHolder.class.getSimpleName();
+  private ItemRecipeListBinding binding;
+  private Context context;
+
+  public FirebaseRecipeViewHolder(ItemRecipeListBinding binding) {
+    super(binding.getRoot());
+    this.binding = binding;
+    this.context = binding.getRoot().getContext();
+  }
+
+  public void bindRecipe(Recipe recipe){
+    Glide.with(context).asBitmap().load(recipe.getImages().getThumbnail().getUrl()).placeholder(R.drawable.brunch_dining).into(binding.recipeImageView);
+    binding.recipeLabel.setText(recipe.getLabel());
+    binding.recipeSource.setText(recipe.getSource());
+    binding.getRoot().setOnClickListener(view -> this.onClick(recipe.getId()));
+  }
+
+  @Override
+  public void onClick(String id) {
+    String userId = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+    DatabaseReference recipeReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RECIPE_LOCATION).child(userId).child(id);
+    recipeReference.addListenerForSingleValueEvent(new ValueEventListener() {
+      @Override
+      public void onDataChange(@NonNull DataSnapshot snapshot) {
+        Intent intent = new Intent(context, RecipeDetailsActivity.class);
+        final String recipeId = Objects.requireNonNull(snapshot.getValue(Recipe.class)).getId();
+        intent.putExtra("recipe_id", recipeId);
+        context.startActivity(intent);
+      }
+
+      @Override
+      public void onCancelled(@NonNull DatabaseError error) {
+        Log.e(TAG, "Error: ", error.toException());
+      }
+    });
+  }
+}
