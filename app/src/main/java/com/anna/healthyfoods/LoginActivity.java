271ccaa0ce1b31c60def7bcad034ff420e007f6b
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-06-12 11:17:55 +0300

    Add form validation implementation

--- app/src/main/java/com/anna/healthyfoods/LoginActivity.java
+++ app/src/main/java/com/anna/healthyfoods/LoginActivity.java
@@ -1,13 +1,15 @@
 package com.anna.healthyfoods;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static com.anna.healthyfoods.utility.Validator.validateEmailInput;
+import static com.anna.healthyfoods.utility.Validator.validatePasswordInput;
 
 import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.anna.healthyfoods.databinding.ActivityLoginBinding;
-import com.anna.healthyfoods.models.Ingredient;
 
 public class LoginActivity extends AppCompatActivity {
   private ActivityLoginBinding binding;
@@ -22,6 +24,7 @@ public class LoginActivity extends AppCompatActivity {
   }
 
   private void setUpLinkToSignUp(){
+    // Add an underline to text
     binding.linkToSignup.setPaintFlags(binding.linkToSignup.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
     binding.linkToSignup.setOnClickListener(view -> {
       startActivity(new Intent(getApplicationContext(), SignupActivity.class));
@@ -29,4 +32,42 @@ public class LoginActivity extends AppCompatActivity {
     });
   }
 
+  private void redirectToUserDetails(){
+    Intent intent = new Intent(getApplicationContext(), UserDetailsActivity.class);
+    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+    startActivity(intent);
+    finish();
+  }
+
+  private boolean validateCredentials(String email, String password) {
+    return isEmailValid(email) && isPasswordValid(password);
+  }
+
+  // Validate email
+  private boolean isEmailValid(String email) {
+    boolean isValid = false;
+
+    if(!validateEmailInput(email)){
+      binding.emailTextInputLayout.setError(getString(R.string.required_valid_email_error));
+    } else {
+      isValid = true;
+      binding.emailTextInputLayout.setErrorEnabled(false);
+    }
+
+    return isValid;
+  }
+
+  // Validate password input
+  private boolean isPasswordValid(String password) {
+    boolean isValid = false;
+
+    if(!validatePasswordInput(password)){
+      binding.passwordTextInputLayout.setError(getString(R.string.required_valid_password_error));
+    } else {
+      isValid = true;
+      binding.passwordTextInputLayout.setErrorEnabled(false);
+    }
+
+    return isValid;
+  }
 }
--- app/src/main/java/com/anna/healthyfoods/SignupActivity.java
+++ app/src/main/java/com/anna/healthyfoods/SignupActivity.java
@@ -1,5 +1,9 @@
 package com.anna.healthyfoods;
 
+import static com.anna.healthyfoods.utility.Validator.validateConfirmPasswordInput;
+import static com.anna.healthyfoods.utility.Validator.validateEmailInput;
+import static com.anna.healthyfoods.utility.Validator.validatePasswordInput;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -18,22 +22,69 @@ public class SignupActivity extends AppCompatActivity {
     setContentView(binding.getRoot());
 
     setUpLinkToLogin();
+
+    binding.btnSignup.setOnClickListener(view -> {
+
+    });
   }
 
   private void setUpLinkToLogin(){
+    // Add an underline to text
     binding.linkToLogin.setPaintFlags(binding.linkToLogin.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
     binding.linkToLogin.setOnClickListener(view -> redirectToLogin());
   }
 
   private void redirectToLogin(){
-    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-    finish();
-  }
-
-  private void redirectToUserDetails(){
-    Intent intent = new Intent(getApplicationContext(), UserDetailsActivity.class);
+    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
     startActivity(intent);
     finish();
   }
+
+  private boolean validateCredentials(String email, String password, String confirmPassword) {
+    return isEmailValid(email) && isPasswordValid(password) && isConfirmPasswordValid(password, confirmPassword);
+  }
+
+  // Validate email
+  private boolean isEmailValid(String email) {
+    boolean isValid = false;
+
+    if(!validateEmailInput(email)){
+      binding.emailTextInputLayout.setError(getString(R.string.required_valid_email_error));
+    } else {
+      isValid = true;
+      binding.emailTextInputLayout.setErrorEnabled(false);
+    }
+
+    return isValid;
+  }
+
+  // Validate password input
+  private boolean isPasswordValid(String password) {
+    boolean isValid = false;
+
+    if(!validatePasswordInput(password)){
+      binding.passwordTextInputLayout.setError(getString(R.string.required_valid_password_error));
+    } else {
+      isValid = true;
+      binding.passwordTextInputLayout.setErrorEnabled(false);
+    }
+
+    return isValid;
+  }
+
+  // Validate confirm password
+  private boolean isConfirmPasswordValid(String password, String confirmPassword) {
+    boolean isValid = false;
+
+    if(!validateConfirmPasswordInput(password, confirmPassword)){
+      binding.confirmPasswordTextInputLayout.setError(getString(R.string.required_confirm_password_error));
+    } else {
+      isValid = true;
+      binding.confirmPasswordTextInputLayout.setErrorEnabled(false);
+    }
+
+    return isValid;
+  }
+
 }
