4dcc1a5eb5f5266959556493485f1528f322d00c
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-06-13 19:14:26 +0300

    Add instrumentation tests for LoginActivity

--- app/src/androidTest/java/com/anna/healthyfoods/LoginActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/anna/healthyfoods/LoginActivityInstrumentationTest.java
@@ -5,6 +5,7 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
 import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -12,14 +13,16 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static com.anna.healthyfoods.custom_matcher.TextInputLayoutMatcher.hasDisplayedErrorText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.*;
 
-import android.app.Activity;
+import android.view.View;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,6 +32,12 @@ import org.junit.runner.RunWith;
 public class LoginActivityInstrumentationTest {
   @Rule
   public ActivityScenarioRule<LoginActivity> loginActivityRule = new ActivityScenarioRule<>(LoginActivity.class);
+  private View activityDecorView;
+
+  @Before
+  public void setUp() {
+    loginActivityRule.getScenario().onActivity(activity -> activityDecorView = activity.getWindow().getDecorView());
+  }
 
   @Test
   public void typeIntoEditText_validateEditText() {
@@ -97,5 +106,35 @@ public class LoginActivityInstrumentationTest {
 
     onView(withId(R.id.btn_login)).perform(click());
     onView(withId(R.id.password_text_input_layout)).check(matches(hasDisplayedErrorText("Password should have at least 8 characters with uppercase, lowercase, digits and special characters")));
+  }
+
+  @Test
+  public void clickingButton_verificationToastDisplaysIfNotVerified() {
+    // Type valid email address
+    onView(allOf(
+            isDescendantOfA(withId(R.id.email_text_input_layout)),
+            withClassName(endsWith("TextInputEditText"))
+    )).perform(typeText("anna@gmail.com")).check(matches(withText("anna@gmail.com"))).perform(closeSoftKeyboard());
+
+    try {
+      Thread.sleep(500);
+    } catch (InterruptedException e){
+      System.out.println("Got interrupted!");
+    }
+
+    // Type valid password
+    onView(allOf(
+            isDescendantOfA(withId(R.id.password_text_input_layout)),
+            withClassName(endsWith("TextInputEditText"))
+    )).perform(typeText("Lanna001$")).check(matches(withText("Lanna001$"))).perform(closeSoftKeyboard());
+
+    try {
+      Thread.sleep(500);
+    } catch (InterruptedException e){
+      System.out.println("Got interrupted!");
+    }
+
+    onView(withId(R.id.btn_login)).perform(click());
+    onView(withText(R.string.email_verification_prompt)).inRoot(withDecorView(not(activityDecorView))).check(matches(withText(R.string.email_verification_prompt)));
   }
 }
