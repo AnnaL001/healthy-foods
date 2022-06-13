14a67182c0741068eaa1e6a216436912b43972db
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-06-13 16:14:56 +0300

    Add instrumentation tests for SignupActivity

---
+++ app/src/androidTest/java/com/anna/healthyfoods/SignupActivityInstrumentationTest.java
@@ -0,0 +1,201 @@
+package com.anna.healthyfoods;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static com.anna.healthyfoods.custom_matcher.TextInputLayoutMatcher.hasDisplayedErrorText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.endsWith;
+
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class SignupActivityInstrumentationTest {
+  @Rule
+  public ActivityScenarioRule<SignupActivity> signUpActivityRule = new ActivityScenarioRule<>(SignupActivity.class);
+
+  @Test
+  public void typeIntoEditText_validateEditText() {
+    // Type valid email address
+    onView(allOf(
+            isDescendantOfA(withId(R.id.email_text_input_layout)),
+            withClassName(endsWith("TextInputEditText"))
+    )).perform(typeText("anna@gmail.com")).check(matches(withText("anna@gmail.com"))).perform(closeSoftKeyboard());
+
+    try {
+      Thread.sleep(500);
+    } catch (InterruptedException e){
+      System.out.println("Got interrupted!");
+    }
+
+    // Type valid password
+    onView(allOf(
+            isDescendantOfA(withId(R.id.password_text_input_layout)),
+            withClassName(endsWith("TextInputEditText"))
+    )).perform(typeText("Lanna001$")).check(matches(withText("Lanna001$"))).perform(closeSoftKeyboard());
+
+    try {
+      Thread.sleep(500);
+    } catch (InterruptedException e){
+      System.out.println("Got interrupted!");
+    }
+
+    // Type valid confirm password
+    onView(allOf(
+            isDescendantOfA(withId(R.id.confirm_password_text_input_layout)),
+            withClassName(endsWith("TextInputEditText"))
+    )).perform(typeText("Lanna001$")).check(matches(withText("Lanna001$"))).perform(closeSoftKeyboard());
+  }
+
+  @Test
+  public void typeInvalidEmail_displaysErrorMessage() {
+    // Type invalid email address
+    onView(allOf(
+            isDescendantOfA(withId(R.id.email_text_input_layout)),
+            withClassName(endsWith("TextInputEditText"))
+    )).perform(typeText("anna")).check(matches(withText("anna"))).perform(closeSoftKeyboard());
+
+    try {
+      Thread.sleep(500);
+    } catch (InterruptedException e){
+      System.out.println("Got interrupted!");
+    }
+
+    onView(withId(R.id.btn_signup)).perform(click());
+    onView(withId(R.id.email_text_input_layout)).check(matches(hasDisplayedErrorText("Please enter a valid email address")));
+  }
+
+  @Test
+  public void typeInvalidPassword_displaysErrorMessage() {
+    // Type valid email address
+    onView(allOf(
+            isDescendantOfA(withId(R.id.email_text_input_layout)),
+            withClassName(endsWith("TextInputEditText"))
+    )).perform(typeText("anna@gmail.com")).check(matches(withText("anna@gmail.com"))).perform(closeSoftKeyboard());
+
+    try {
+      Thread.sleep(500);
+    } catch (InterruptedException e){
+      System.out.println("Got interrupted!");
+    }
+
+    // Type invalid password
+    onView(allOf(
+            isDescendantOfA(withId(R.id.password_text_input_layout)),
+            withClassName(endsWith("TextInputEditText"))
+    )).perform(typeText("12345678")).check(matches(withText("12345678"))).perform(closeSoftKeyboard());
+
+    try {
+      Thread.sleep(500);
+    } catch (InterruptedException e){
+      System.out.println("Got interrupted!");
+    }
+
+    onView(withId(R.id.btn_signup)).perform(click());
+    onView(withId(R.id.password_text_input_layout)).check(matches(hasDisplayedErrorText("Password should have at least 8 characters with uppercase, lowercase, digits and special characters")));
+  }
+
+  @Test
+  public void typeInvalidConfirmPassword_displaysErrorMessage() {
+    // Type valid email address
+    onView(allOf(
+            isDescendantOfA(withId(R.id.email_text_input_layout)),
+            withClassName(endsWith("TextInputEditText"))
+    )).perform(typeText("anna@gmail.com")).check(matches(withText("anna@gmail.com"))).perform(closeSoftKeyboard());
+
+    try {
+      Thread.sleep(500);
+    } catch (InterruptedException e){
+      System.out.println("Got interrupted!");
+    }
+
+    // Type valid password
+    onView(allOf(
+            isDescendantOfA(withId(R.id.password_text_input_layout)),
+            withClassName(endsWith("TextInputEditText"))
+    )).perform(typeText("Lanna001$")).check(matches(withText("Lanna001$"))).perform(closeSoftKeyboard());
+
+    try {
+      Thread.sleep(500);
+    } catch (InterruptedException e){
+      System.out.println("Got interrupted!");
+    }
+
+    // Type invalid confirm password
+    onView(allOf(
+            isDescendantOfA(withId(R.id.confirm_password_text_input_layout)),
+            withClassName(endsWith("TextInputEditText"))
+    )).perform(typeText("12345678")).check(matches(withText("12345678"))).perform(closeSoftKeyboard());
+
+    try {
+      Thread.sleep(500);
+    } catch (InterruptedException e){
+      System.out.println("Got interrupted!");
+    }
+
+    onView(withId(R.id.btn_signup)).perform(click());
+    onView(withId(R.id.confirm_password_text_input_layout)).check(matches(hasDisplayedErrorText("Confirm password should match password")));
+  }
+
+  @Test
+  public void clickButton_redirectsToLoginAfterSignup() {
+    // Type valid email address
+    onView(allOf(
+            isDescendantOfA(withId(R.id.email_text_input_layout)),
+            withClassName(endsWith("TextInputEditText"))
+    )).perform(typeText("anna@gmail.com")).check(matches(withText("anna@gmail.com"))).perform(closeSoftKeyboard());
+
+    try {
+      Thread.sleep(500);
+    } catch (InterruptedException e){
+      System.out.println("Got interrupted!");
+    }
+
+    // Type valid password
+    onView(allOf(
+            isDescendantOfA(withId(R.id.password_text_input_layout)),
+            withClassName(endsWith("TextInputEditText"))
+    )).perform(typeText("Lanna001$")).check(matches(withText("Lanna001$"))).perform(closeSoftKeyboard());
+
+    try {
+      Thread.sleep(500);
+    } catch (InterruptedException e){
+      System.out.println("Got interrupted!");
+    }
+
+    // Type valid confirm password
+    onView(allOf(
+            isDescendantOfA(withId(R.id.confirm_password_text_input_layout)),
+            withClassName(endsWith("TextInputEditText"))
+    )).perform(typeText("Lanna001$")).check(matches(withText("Lanna001$"))).perform(closeSoftKeyboard());
+
+    Intents.init();
+    onView(withId(R.id.btn_signup)).perform(click());
+
+    // Sleep while user is signed up
+    try {
+      Thread.sleep(2000);
+    } catch (InterruptedException e){
+      System.out.println("Got interrupted!");
+    }
+
+    // Confirm user is redirected to login
+    Intents.intended(hasComponent(LoginActivity.class.getName()));
+  }
+
+}
