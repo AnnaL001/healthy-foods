e1549d1d8a2910051e36cc67c130d4a40b4c734f
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-06-13 12:10:52 +0300

    Add functionality to redirect to home if already logged in

--- app/src/main/java/com/anna/healthyfoods/MainActivity.java
+++ app/src/main/java/com/anna/healthyfoods/MainActivity.java
@@ -1,22 +1,28 @@
 package com.anna.healthyfoods;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.anna.healthyfoods.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
   public static final String TAG = MainActivity.class.getSimpleName();
   private ActivityMainBinding binding;
+  private FirebaseAuth auth;
+  private FirebaseAuth.AuthStateListener authStateListener;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     binding = ActivityMainBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
+
+    auth = FirebaseAuth.getInstance();
+    initializeAuthStateListener();
 
     handleButtonClick();
   }
@@ -27,5 +33,32 @@ public class MainActivity extends AppCompatActivity {
       startActivity(new Intent(getApplicationContext(), LoginActivity.class));
       finish();
     });
+  }
+
+  private void initializeAuthStateListener(){
+    authStateListener = firebaseAuth -> {
+      if(firebaseAuth.getCurrentUser() != null){
+        redirectToHome();
+      }
+    };
+  }
+
+  private void redirectToHome(){
+    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+    startActivity(intent);
+    finish();
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    auth.addAuthStateListener(authStateListener);
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    auth.removeAuthStateListener(authStateListener);
   }
 }
