4e9d18ac2fe751de59caf918bd8aa8f8132ee6d5
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-06-13 11:45:06 +0300

    Add logout functionality

--- app/src/main/java/com/anna/healthyfoods/HomeActivity.java
+++ app/src/main/java/com/anna/healthyfoods/HomeActivity.java
@@ -1,19 +1,19 @@
 package com.anna.healthyfoods;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-import android.os.Bundle;
-import android.util.Log;
-
 import com.anna.healthyfoods.adapter.HomeViewPagerAdapter;
 import com.anna.healthyfoods.databinding.ActivityHomeBinding;
 import com.anna.healthyfoods.models.Settings;
-import com.anna.healthyfoods.ui.MealTypeFragment;
-import com.anna.healthyfoods.ui.SearchFragment;
 import com.anna.healthyfoods.utility.Constants;
-import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -21,8 +21,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import org.parceler.Parcels;
 
 import java.util.Objects;
 
@@ -33,6 +31,8 @@ public class HomeActivity extends AppCompatActivity {
   private FragmentStateAdapter viewPagerAdapter;
   private DatabaseReference settingsReference;
   private ValueEventListener settingsListener;
+  private FirebaseAuth auth;
+  private FirebaseAuth.AuthStateListener authStateListener;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +40,10 @@ public class HomeActivity extends AppCompatActivity {
     binding = ActivityHomeBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
 
-    String userId = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+    auth = FirebaseAuth.getInstance();
+    initializeAuthStateListener();
+
+    String userId = Objects.requireNonNull(auth.getCurrentUser()).getUid();
     settingsReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_SETTINGS_LOCATION).child(userId);
 
     setUpHomeScreen();
@@ -80,8 +83,53 @@ public class HomeActivity extends AppCompatActivity {
   }
 
   @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.app_menu, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    if(item.getItemId() == R.id.action_logout) {
+      logout();
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
+  private void logout(){
+    auth.signOut();
+  }
+
+  private void initializeAuthStateListener(){
+    authStateListener = firebaseAuth -> {
+      if(firebaseAuth.getCurrentUser() == null){
+        redirectToLaunchScreen();
+      }
+    };
+  }
+
+  private void redirectToLaunchScreen(){
+    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+    startActivity(intent);
+    finish();
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    auth.addAuthStateListener(authStateListener);
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    auth.addAuthStateListener(authStateListener);
+  }
+
+  @Override
   protected void onDestroy() {
-    settingsReference.removeEventListener(settingsListener);
     super.onDestroy();
+    settingsReference.removeEventListener(settingsListener);
   }
 }
