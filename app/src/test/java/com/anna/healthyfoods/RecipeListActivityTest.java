8c0c451df9726bd5b610fcaf66663382d6a168bc
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-06-14 14:15:06 +0300

    Add and update unit tests

--- app/src/test/java/com/anna/healthyfoods/RecipeListActivityTest.java
+++ app/src/test/java/com/anna/healthyfoods/RecipeListActivityTest.java
@@ -1,8 +1,12 @@
 package com.anna.healthyfoods;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.robolectric.Shadows.shadowOf;
 
 import android.content.Intent;
+import android.view.View;
 
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.core.app.ApplicationProvider;
@@ -19,10 +23,12 @@ import org.robolectric.RobolectricTestRunner;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 @RunWith(RobolectricTestRunner.class)
 public class RecipeListActivityTest {
   private RecipeListActivity activity;
+  private RecyclerView recyclerView;
 
   @Before
   public void setUp()  {
@@ -41,11 +47,46 @@ public class RecipeListActivityTest {
             .start()
             .resume()
             .get();
+    recyclerView = activity.findViewById(R.id.recipe_list);
   }
 
   @Test
   public void validateRecyclerView() {
-    RecyclerView recyclerView = activity.findViewById(R.id.recipe_list);
     assertNotNull(recyclerView);
+  }
+
+  @Test
+  public void validateDataReturned() {
+    sleep();
+    assertNotEquals(0, Objects.requireNonNull(recyclerView.getAdapter()).getItemCount());
+  }
+
+  @Test
+  public void validateDataTypeReturned() {
+    sleep();
+
+    // Layout recyclerview with specified dimensions so that viewholders are inflated
+    recyclerView.measure(
+            View.MeasureSpec.UNSPECIFIED,
+            View.MeasureSpec.UNSPECIFIED
+    );
+    recyclerView.layout(0, 0, 1000, 1000);
+
+    RecyclerView.ViewHolder recipeViewHolder = recyclerView.findViewHolderForAdapterPosition(0);
+    Objects.requireNonNull(recipeViewHolder).itemView.performClick();
+
+    Intent expectedIntent = new Intent(activity, RecipeDetailsActivity.class);
+    expectedIntent.putExtra("recipe_id", "recipe_4bb99424e1bbc40d3cd1d891883d6745");
+    Intent actualIntent = shadowOf(activity).getNextStartedActivity();
+
+    assertEquals(expectedIntent.getStringExtra("recipe_id"), actualIntent.getStringExtra("recipe_id"));
+  }
+
+  private void sleep(){
+    try {
+      Thread.sleep(10000);
+    } catch (InterruptedException e){
+      System.out.println("Got interrupted!");
+    }
   }
 }
