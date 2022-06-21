fdab4e0bdc1d5ddaf8b23c9f72f14a91d6bce060
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-06-21 19:59:36 +0300

    Update recipe list screen layout and logic

--- app/src/main/java/com/anna/healthyfoods/RecipeListActivity.java
+++ app/src/main/java/com/anna/healthyfoods/RecipeListActivity.java
@@ -24,10 +24,7 @@ import com.anna.healthyfoods.interfaces.EdamamApi;
 import com.anna.healthyfoods.interfaces.ItemOnClickListener;
 import com.anna.healthyfoods.models.RecipeSearchResponse;
 import com.anna.healthyfoods.models.Settings;
-import com.anna.healthyfoods.ui.RecipeDetailsFragment;
-import com.anna.healthyfoods.ui.RecipeListFragment;
 import com.anna.healthyfoods.utility.Constants;
-import com.anna.healthyfoods.utility.UserInterfaceHelpers;
 
 import org.parceler.Parcels;
 
@@ -37,12 +34,10 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class RecipeListActivity extends AppCompatActivity {
+public class RecipeListActivity extends AppCompatActivity implements ItemOnClickListener {
   public static final String TAG = RecipeListActivity.class.getSimpleName();
   private ActivityRecipeListBinding binding;
   private RecipeListAdapter adapter;
-  private String mealType;
-  private Settings userSettings;
 
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -50,17 +45,71 @@ public class RecipeListActivity extends AppCompatActivity {
     binding = ActivityRecipeListBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
 
-    mealType = getIntent().getStringExtra(Constants.EXTRA_MEAL_TYPE);
-    userSettings = Parcels.unwrap(getIntent().getParcelableExtra(Constants.EXTRA_USER_SETTINGS));
+    String mealType = getIntent().getStringExtra(Constants.EXTRA_MEAL_TYPE);
+    Settings userSettings = Parcels.unwrap(getIntent().getParcelableExtra(Constants.EXTRA_USER_SETTINGS));
 
-    inflateFragment();
+    // Set action bar title to passed meal type
+    Objects.requireNonNull(getSupportActionBar()).setTitle(mealType);
+
+    // Create arrays for diet and health preferences for recipes
+    String[] diets = new String[userSettings.getDiets().size()];
+    String[] preferences = new String[userSettings.getPreferences().size()];
+
+    EdamamApi client = EdamamClient.getClient();
+    Call<RecipeSearchResponse> call = client.getRecipesByMealType("public", "", Constants.EDAMAM_API_ID, Constants.EDAMAM_API_KEY, mealType, userSettings.getDiets().toArray(diets), userSettings.getPreferences().toArray(preferences));
+
+    loadRecipes(call);
   }
 
-  // Inflate RecipeListFragment with extras
-  private void inflateFragment(){
-    getSupportFragmentManager().beginTransaction()
-            .setReorderingAllowed(true)
-            .add(R.id.fragment_container, RecipeListFragment.newInstance(mealType, userSettings))
-            .commit();
+  private void loadRecipes(Call<RecipeSearchResponse> call){
+    call.enqueue(new Callback<RecipeSearchResponse>() {
+      @Override
+      public void onResponse(@NonNull Call<RecipeSearchResponse> call, @NonNull Response<RecipeSearchResponse> response) {
+        hideProgressDialog(binding.progressBar, binding.progressMessage);
+
+        if(response.isSuccessful()){
+          assert response.body() != null;
+          adapter = new RecipeListAdapter(getApplicationContext(), response.body().getHits(), RecipeListActivity.this);
+
+          setLayoutManager();
+
+          binding.recipeList.setAdapter(adapter);
+
+          if(adapter.getItemCount() > 0){
+            showRecipes(binding.recipeList);
+          } else {
+            showNoContentFound(binding.errorText, getString(R.string.no_recipes_found));
+          }
+        } else {
+          showUnsuccessfulFeedback(binding.errorText, getApplicationContext());
+        }
+      }
+
+      @Override
+      public void onFailure(@NonNull Call<RecipeSearchResponse> call, @NonNull Throwable t) {
+        hideProgressDialog(binding.progressBar, binding.progressMessage);
+        showFailureFeedback(binding.errorText, getApplicationContext());
+        Log.e(TAG, "Error: ", t);
+      }
+    });
+  }
+
+  private void setLayoutManager(){
+    // Set layout manager based on orientation
+    if(binding.getRoot().getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+      binding.recipeList.setLayoutManager(new GridLayoutManager(this, 2));
+    } else {
+      binding.recipeList.setLayoutManager(new LinearLayoutManager(this));
+    }
+  }
+
+
+  @Override
+  public void onClick(String id, boolean isSaved) {
+    Intent intent = new Intent(this, RecipeDetailsActivity.class);
+    intent.putExtra(Constants.EXTRA_RECIPE_ID, id);
+    intent.putExtra(Constants.EXTRA_SAVED, isSaved);
+    Log.d(TAG, "Recipe ID: " + id);
+    startActivity(intent);
   }
 }
